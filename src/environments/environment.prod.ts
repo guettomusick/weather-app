669d0b5fe793ce34d255448e457538fb90bb616a
guettomusick <guettomusick@gmail.com>
2019-04-29 15:46:21 -0300

    Adapt for stackblitz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   weatherForecast: {
-    apiUrl: 'https://samples.openweathermap.org/data/2.5/forecast',
-    apiKey: 'b6907d289e10d714a6e88b30761fae22'
+    apiUrl: 'https://api.openweathermap.org/data/2.5/forecast/hourly',
+    apiKey: '20a18b996433085c5d3095a8f620426e'
   },
   googleMaps: {
     apiUrl: 'https://maps.googleapis.com/maps/api/timezone/json',
